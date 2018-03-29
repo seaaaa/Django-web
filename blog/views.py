170b11abe09bdbe6e1ea0663867bd2d715f20da5
will <will@it-c-2.local.lan>
2018-03-29 17:34:17 +0800

    added database

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from . models import Comment
 def home_page(request):
-     return render(request, 'blog/home_page.html')
+    if request.method == 'POST':
+         our_comment = request.POST.get('comment')
+         our_user = request.POST.get("username")
+         comment_obj = Comment.objects.create(
+             comment=our_comment,
+             user=our_user
+         )
+         print(our_comment * 10)
+         return render(request, 'blog/home_page.html', {'comment': comment_obj,})
+    return render(request, 'blog/home_page.html')
